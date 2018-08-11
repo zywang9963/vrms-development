06ef9769a33a94cf3f768d9563e8cffe63a37582
steven <zy_w_6@163.com>
2018-08-11 23:50:45 +0800

    update 2

--- python-desktop/vrms/src/StartApp.py
+++ python-desktop/vrms/src/StartApp.py
@@ -6,13 +6,13 @@ from vrms.views.main.GuiManager import GuiManager
 class App(wx.App):
     def OnInit(self):
         self.manager = GuiManager(self.UpdateUI)
-        self.frame = self.manager.GetFrame(1, None)
+        self.frame = self.manager.GetFrame(1, "en_US")
         self.frame.Show(True)
         return True
 
     def UpdateUI(self, type):
         self.frame.Show(False)
-        self.frame = self.manager.GetFrame(type, None)
+        self.frame = self.manager.GetFrame(type, "en_US")
         self.frame.Show(True)
 
 
